dbaae1df57b6573f1c2d2f355259ade3119cb417
sergio-contente <sergio.mcontente@gmail.com>
2022-02-07 14:52:07 -0300

    ♻️ Resolve iteration errors

--- ex1.py
+++ ex1.py
@@ -9,6 +9,7 @@ def plot_aproximations(iterations, error_autovalor, error_autovetor, error_assin
 def main():
 	list_x0 = []
 	list_B = []
+	itmax = np.random.randint(30, 71)
 	for i in range(0, 3):
 		value_x0 = np.random.rand()
 		list_x0.append(value_x0)
@@ -21,12 +22,11 @@ def main():
 		#A_t = np.transpose(A)
 		#B = np.add(A, A_t)
 	A = np.array([[-2, -4, 2], [-2, 1, 2], [4, 2, 5]])
-	metodo = metodo_potencia(vector_x0, A, 10)
-	valor, vetor = metodo.get_autovetor_autovalor()
-	print(valor[valor.size - 1])
+	metodo = metodo_potencia(vector_x0, A, itmax, 10)
+	for i in range(1, itmax):
+		metodo.autovalor_autovetor_error()
+	#valor, vetor = metodo.get_autovetor_autovalor()
 	#print(metodo.get_microk())
-	print(metodo.autovalor_autovetor_error())
-	print()
 	pass
 if __name__ == '__main__':
 	main()
--- metodos/potencias.py
+++ metodos/potencias.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 class metodo_potencia:
-    def __init__(self, x_0, A, n):
+    def __init__(self, x_0, A, itmax, n):
         self.x_k = x_0
         self.matriz = A
-        self.itmax = np.random.randint(30, 71)
+        self.itmax = itmax
+        print(f"itmax: {self.itmax}")
         self.n = n
 
     def get_xk(self):
-        for i in range(1, self.itmax):
-            self.x_k = np.dot(self.matriz, self.x_k)/np.linalg.norm((np.dot(self.matriz, self.x_k)))
+        self.x_k = np.dot(self.matriz, self.x_k)/np.linalg.norm((np.dot(self.matriz, self.x_k)))
         return self.x_k
     def get_microk(self):
         xk_t = np.transpose(self.get_xk())
@@ -19,10 +19,12 @@ class metodo_potencia:
     def autovalor_autovetor_error(self):
         autovalor, autovetor = self.get_autovetor_autovalor()
         error_autovetor = np.linalg.norm(self.get_xk() - autovetor)
+        print(f"iterator: {self.itmax}")
         print(f"autovetor_error: {error_autovetor}")
-        error_valor =np.abs(self.get_microk() - autovalor[autovalor.size - 1])
+        error_valor =np.abs(self.get_microk() - np.round(autovalor[autovalor.size - 1]))
         print(f"autovalor_error: {error_valor}")
-        print(np.power(autovalor[autovalor.size - 2]/autovalor[autovalor.size - 1], self.itmax))
+        print(f"microk: {self.get_microk()}")
+        print(np.power((autovalor[autovalor.size - 2]/autovalor[autovalor.size - 1]), self.itmax))
 
     def get_autovetor_autovalor(self):
         lambdas, autovetores = np.linalg.eig(self.matriz)
