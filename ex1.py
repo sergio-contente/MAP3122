efd53c32b66adc7fdeedc7f08b54ee8643e0b6f7
sergio-contente <sergio.mcontente@gmail.com>
2022-02-07 14:02:06 -0300

    ✨ Calculating micro_k

--- ex1.py
+++ ex1.py
@@ -1,10 +1,15 @@
 from metodos.potencias import metodo_potencia
 import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_aproximations(iterations, error_autovalor, error_autovetor, error_assintotico, error_assintotico_quadrado):
+	pass
+
 
 def main():
 	list_x0 = []
 	list_B = []
-	for i in range(0, 10):
+	for i in range(0, 3):
 		value_x0 = np.random.rand()
 		list_x0.append(value_x0)
 		vector_x0 = np.array([[j] for j in list_x0])
@@ -12,17 +17,13 @@ def main():
 
 		#value_B = np.random.rand()
 		#list_B.append(value_B)
-		A = np.random.rand(10,10)
-		A_t = np.transpose(A)
-		B = np.add(A, A_t)
-	print(A[0])
-	print(A_t[0])
-	print(B[0])
-	print(B.shape)
-
+		#A = np.random.rand(10,10)
+		#A_t = np.transpose(A)
+		#B = np.add(A, A_t)
+	A = np.array([[-2, -4, 2], [-2, 1, 2], [4, 2, 5]])
 	metodo = metodo_potencia(vector_x0, A, 10)
 	valor, vetor = metodo.get_autovetor_autovalor()
-	
+	print(metodo.get_microk())
 	pass
 if __name__ == '__main__':
 	main()
--- metodos/potencias.py
+++ metodos/potencias.py
@@ -1,17 +1,22 @@
 import numpy as np
 
 class metodo_potencia:
-    def __init__(self, autovetor, A, n):
-        self.autovetor = autovetor
+    def __init__(self, x_0, A, n):
+        self.x_k = x_0
         self.matriz = A
         self.itmax = np.random.randint(30, 71)
         self.n = n
 
     def get_xk(self):
         for i in range(1, self.itmax):
-            self.autovetor = np.dot(self.matriz, self.autovetor)/np.linalg.norm((np.dot(self.autovetor, self.matriz)))
-        return self.autovetor
-        
+            self.x_k = np.dot(self.matriz, self.x_k)/np.linalg.norm((np.dot(self.matriz, self.x_k)))
+        return self.x_k
+    def get_microk(self):
+        xk_t = np.transpose(self.get_xk())
+        A_xk = np.dot(self.matriz, self.x_k)
+        microk = np.dot(xk_t, A_xk)/np.dot(xk_t, self.x_k)
+        return microk
+
     def get_autovetor_autovalor(self):
         lambdas, autovetores = np.linalg.eig(self.matriz)
         return lambdas, autovetores
