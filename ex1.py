53c5004d912dc9471ebc5d7715ce13bf4cc6805f
sergio-contente <sergio.mcontente@gmail.com>
2022-02-16 18:23:54 -0300

    🚧  Need to fix eigenvector

--- ex1.py
+++ ex1.py
@@ -5,39 +5,42 @@ import matplotlib.pyplot as plt
 def plot_aproximations(iterations, error_autovalor, error_autovetor, error_assintotico, error_assintotico_quadrado):
 	pass
 
+def generate_x0(n):
+        list_x0 = []
+        for i in range(0, n):
+            value_x0 = np.random.rand()
+            list_x0.append(value_x0)
+        vector_x0 = np.array([[j] for j in list_x0])
+        np.transpose(vector_x0)
+        return vector_x0
 
 def main():
+	n = 3
 	x_values = []
 	error_autovalor = []
 	error_autovetor = []
-	list_x0 = []
 	list_B = []
-	itmax = np.random.randint(30, 71)
-	for i in range(0, 3):
-		value_x0 = np.random.rand()
-		list_x0.append(value_x0)
-		vector_x0 = np.array([[j] for j in list_x0])
-		np.transpose(vector_x0)
-
+	itmax = 30 #np.random.randint(30, 71)
+	vector_x0 = generate_x0(n)
 		#value_B = np.random.rand()
 		#list_B.append(value_B)
 		#A = np.random.rand(10,10)
 		#A_t = np.transpose(A)
 		#B = np.add(A, A_t)
 	A = np.array([[-2, -4, 2], [-2, 1, 2], [4, 2, 5]])
-	metodo = metodo_potencia(vector_x0, A, itmax, 10)
+	metodo = metodo_potencia(vector_x0, A, itmax, n)
 	for i in range(1, itmax):
 		x_values.append(i)
-		autovalor, autovetor = metodo.autovalor_autovetor_error()
+		autovalor = metodo.autovalor_autovetor_error()
 		autovalor = autovalor.reshape(-1)
-		autovetor = autovetor.reshape(-1)
+		#autovetor = autovetor.reshape(-1)
 		#print(type(autovalor))
 		#list_autovalor = autovalor.tolist()
 		#print(type(list_autovalor))
 		#list(list_autovalor)
 		#print(list_autovalor)
 		error_autovalor.append(autovalor)
-		error_autovetor.append(autovetor)
+		#error_autovetor.append(autovetor)
 	assint_values = metodo.get_assintotico()
 	#print(type(x_values))
 	#print(assint_values)
@@ -45,7 +48,7 @@ def main():
 	plt.yscale("log")
 	plt.plot(x_values, assint_values, label="assint^n")
 	plt.plot(x_values, error_autovalor, label="error autovalor")
-	plt.plot(x_values, error_autovetor, label="error autovetor")
+	#plt.plot(x_values, error_autovetor, label="error autovetor")
 	plt.legend()
 	plt.show()
 	
--- metodos/potencias.py
+++ metodos/potencias.py
@@ -11,22 +11,24 @@ class metodo_potencia:
     def get_xk(self):
         self.x_k = np.dot(self.matriz, self.x_k)/np.linalg.norm((np.dot(self.matriz, self.x_k)))
         return self.x_k
+
     def get_microk(self):
         xk_t = np.transpose(self.get_xk())
         A_xk = np.dot(self.matriz, self.x_k)
         microk = np.dot(xk_t, A_xk)/np.dot(xk_t, self.x_k)
         return microk
+    
     def autovalor_autovetor_error(self):
         autovalor, autovetor = self.get_autovetor_autovalor()
-        error_autovetor = np.linalg.norm(self.get_xk() - autovetor[autovalor.size - 1])
-        print(autovetor[autovalor.size - 1])
+        #error_autovetor = np.linalg.norm(self.get_xk() - autovetor[autovalor.size - 1])
+        #print(autovetor[autovalor.size - 1])
         #print(f"iterator: {self.itmax}")
         #print(f"autovetor_error: {error_autovetor}")
         error_autovalor =np.abs(self.get_microk() - np.round(autovalor[autovalor.size - 1]))
         #error_autovalor_list = error_autovalor.tolist()
         #print(f"autovalor_error: {error_valor}")
         #print(f"microk: {self.get_microk()}")
-        return error_autovalor, error_autovetor
+        return error_autovalor# error_autovetor
 
     def get_assintotico(self):
         vetor = []
@@ -41,7 +43,26 @@ class metodo_potencia:
         return vetor
 
     def get_autovetor_autovalor(self):
-        lambdas, autovetores = np.linalg.eig(self.matriz)
-        lambdas = np.sort(np.abs(lambdas))
-        return lambdas, autovetores
+        lambdas_old, autovetores = np.linalg.eig(self.matriz)
+        print(f"ANTIGO LAMBDA: {lambdas_old}")
+        print(f"ANTIGO VETORES: {autovetores}")
+        lambdas = np.sort(np.abs(lambdas_old))
+        vetores = np.empty(autovetores.shape)
+        for j in range(0, lambdas_old.size):
+            for i in range(0, autovetores[j].size):
+                if (np.dot(self.matriz, autovetores[j][i]) - np.dot(lambdas_old[j], autovetores[j][i]) == 0).all():
+                    index_lambda = lambdas.np.where(lambdas == np.abs(lambdas_old[j]))
+                    vetores[index_lambda] = autovetores[i]
+        print(f"NOVO LAMBDA: {lambdas}")
+        print(f"NOVO VETORES: {vetores}")
+        return lambdas, vetores
 
+        #lambda_1 = lambdas[lambdas.size-1]
+        #for autovetor in autovetores:
+        #   if (np.dot(self.matriz, autovetor) - np.dot(lambda_key, autovetor) == 0).all():
+        #       
+        #
+        #
+        #
+        #
+        #
