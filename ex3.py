e2dceb5fb50dfd25255b25079fbe29ea8012a733
sergio-contente <sergio.mcontente@gmail.com>
2022-02-20 01:29:42 -0300

    🚧  AAAA

--- ex3.py
+++ ex3.py
@@ -20,22 +20,23 @@ def matrix_2(n):
 	return A
 def main():
 	#A = np.array([[12,-51,4], [6,167,-68], [-4,24,-41]])
-	#A = np.array([[1,-1,4],[1,4,-2],[1,4,2]])
-	#A = np.array([[2. , 5., 11.],[5. , 4., 17.],[11., 17., 6.]])
-	A = np.array([[6. , -2., -1.],[-2. , 6., -1.],[-1., -1., 5.]])
+	A = np.array([[4.,-14.,-12.],[-14.,10.,17.],[-12.,17.,1.]])
+	#A = np.array([[5. , -2.],[-2. , 8.]])
+	#A = np.array([[3.,2., 4.],[2.,0.,2.],[4.,2.,3.]]) #lambdas iguais
+	#A = np.array([[6. , -2., -1.],[-2. , 6., -1.],[-1., -1., 5.]])
 
 	V_k = np.identity(A.shape[1])
-	A_k = A
-	it_max = 10
-	symmetrical = is_symmetrical(A)
+	A_copy = np.array(A, copy=True)
+	autovalor, autovetor = np.linalg.eig(A_copy)
+	A_k = np.array(A, copy=True)
+	it_max = 700
+	symmetrical = is_symmetrical(A_copy)
 	converged = False
 	for iteration in range(1, it_max):
 		qr = fatoracao_qr(A_k)
 		Q_k, R_k = qr.get_qr()
 		if symmetrical:
-			print(f"V_k * Q_k:\n{V_k}\n{Q_k}")
-			V_k = np.matmul(V_k,Q_k)
-			print(f"V_k:\n{V_k}")
+			V_k = np.matmul(V_k, Q_k)
 		A_k = np.matmul(R_k,Q_k)
 		failed_conversion = False
 		for i in range(A_k.shape[0]):
@@ -43,17 +44,17 @@ def main():
 				if i > j and np.abs(A_k[i][j]) > epsilon:
 					failed_conversion = True
 					break
-			if i == (A_k.shape[0] - 1) and failed_conversion == False:
-				converged = True
-		if converged:
-			break
-	autovalor, autovetor = np.linalg.eig(A_k)
-	for c in range(V_k.shape[1]):
-		V_k[:,[c]] = V_k[:,[c]]/np.linalg.norm(V_k[:,[c]])
-		#autovetor[:, [c]] = autovetor[:, [c]]/np.linalg.norm(autovetor[:, [c]])
+		# 	if i == (A_k.shape[0] - 1) and failed_conversion == False:
+		# 		converged = True
+		# 		print("CONVERGIU!")
+		# if converged:
+		# 	break
 	
 	print(f"Autovalores reais:\n{autovalor}\nAutovalores aproximados:\n{A_k}")
-	print(f"Autovetores reais:\n{autovetor}\nAutovetores aproximados:\n{V_k}")
+	print(f"Autovetores reais:\n{autovetor}\nAutovetores aproximados:\n{V_k}\nA:\n{A_copy}\nA_k:\n{A_k}")
+	print(f"Return da eig:\n{np.linalg.eig(A)}")
+
+
 if __name__ == '__main__':
 	main()
 
--- metodos/fatoracao_qr.py
+++ metodos/fatoracao_qr.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 class fatoracao_qr:
 	def __init__(self, A):
-		self.A_0 = A
-		self.A_k = A
+		self.A_0 = np.array(A, copy=True)
+		self.A_k = np.array(A, copy=True)
 		self.tamanho = A.shape[1]
-		self.q_teste = self.A_0
-		self.R = A
+		self.q_teste = np.array(self.A_0)
+		self.R = np.array(A, copy=True)
 		self.Q = np.identity(self.A_0.shape[1])
 
 	def get_v(self, A_k):
-		x = A_k[:, [0]]
+		x = np.array(A_k[:, [0]], copy=True)
 		if x[0][0] < 0:
 			delta = -1
 		else:
@@ -25,7 +25,7 @@ class fatoracao_qr:
 		#print(f"e:\n{e}")
 		v = x + delta*norm_x*e
 		v = np.transpose(v)
-		return v
+		return np.array(v)
 
 	def get_H_k(self, v):
 		vt = np.transpose(v)
@@ -35,34 +35,34 @@ class fatoracao_qr:
 		identidade = np.identity(v.shape[0])
 		#print(f"identidade: {identidade}")
 		H = identidade - 2*vvt
-		return H
+		return np.array(H)
 
 	def update_A_k(self):
 		self.A_k = np.matmul(self.Q, self.R)
-		return self.A_k
+		return np.array(self.A_k)
 
 	def update_Q_k(self, H_k):
 		size = H_k.shape[1]
 		i = self.tamanho - size
 		temp = np.matmul(self.Q[i:, i:], H_k)
-		self.Q[i:,i:] = temp
+		self.Q[i:,i:] = np.array(temp, copy=True)
 		#print(f"Q:\n{self.Q}")
-		return self.Q
+		return np.array(self.Q)
 
 	def update_R_k(self, H_k):
 		size = H_k.shape[1]
 		i = self.tamanho - size
 		temp = np.matmul(H_k, self.R[i:, i:])
-		self.R[i:,i:] = temp
+		self.R[i:,i:] = np.array(temp, copy=True)
 		#print(f"R:\n{self.R}")
-		return self.R
+		return np.array(self.R)
 
 	def get_qr(self):
-		R = self.A_0
+		R = np.array(self.A_0, copy=True)
 		for i in range(self.A_0.shape[1] - 1):
 			v = self.get_v(R[i:, i:])
 			H = self.get_H_k(v)
 			Q = self.update_Q_k(H)
 			R = self.update_R_k(H)
-		return Q, R
+		return np.array(Q), np.array(R)
 		
