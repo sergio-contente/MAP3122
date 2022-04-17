40ebebc196bfc70e4ddd778837d6eefe988a1649
Jonas <jonasgomesdemorais@gmail.com>
2022-04-17 12:02:14 -0300

    🚧

--- EP2/options.py
+++ EP2/options.py
@@ -1,17 +1,22 @@
 from cmath import tau
 from metodos import *
 
-def option(N, L, sigma, K, T, r, t, S0, St, vetorizar=True):
-    M = N + L
+def option(N, L, sigma, K, T, r, t, S0, vetorizar=True):
+    M = L * 2
 
     u = iterador_u_ij(N, L, sigma, K, T, vetorizar)
+    tau = get_tau_analitico(t, T)
+    x = get_x_analitico(S0, K, r, sigma, tau)
+    u_analitico = get_u_analitico_normal(x, sigma, tau, K)
+    for j in range(M+1):
+        for i in range(N+1):
+            print("u_ij numerico = " + u[i][j])
+            print("u_ij analitico = " + u_analitico[i][j])
+
     V_aprox = get_V_aprox(M, N, L, sigma, K, T, r, u, vetorizar)
     V_interpol = get_V_interpol(M, N, L, t, T, K, r, sigma, V_aprox)
 
     # S = get_S_analitico()
-    tau = get_tau_analitico(t, T)
-    x = get_x_analitico(St, K, r, sigma, tau)
-    u_analitico = get_u_analitico_normal(x, sigma, tau, K)
 
 
 def main():
@@ -23,9 +28,9 @@ def main():
     r = float(input("Insira o valor de r: "))
     t = float(input("Insira o valor de t: "))
     S0 = float(input("Insira o valor de S0: "))
-    S1 = float((input("Insira o valor de S1: ")))
+    St = float((input("Insira o valor de S1: ")))
 
-    option(N, L, sigma, K, T, r, t, S0, S1)
+    option(N, L, sigma, K, T, r, t, S0, St)
 
 if __name__ == '__main__':
     try:
