921672e5083a73504d6128fe4b7ba52931406723
Jonas <jonasgomesdemorais@gmail.com>
2022-04-17 20:58:23 -0300

    Co-authored-by: Sergio Contente <sergio-contente@users.noreply.github.com>

--- EP2/metodos.py
+++ EP2/metodos.py
@@ -1,46 +1,53 @@
 import statistics
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 def get_tau_analitico(t, T):
     tau = T-t
     return tau
 
 def get_delta_x_analitico(L, N):
-    delta_x = 2*L/N
+    delta_x = float(2*L/N)
     return delta_x
 
 def get_delta_tau_analitico(T, M):
-    delta_tau = T/M
+    delta_tau = float(T/M)
     return delta_tau
 
 def get_x_analitico(S, K, r, sigma, tau): # ou update?
     x = np.log(S/K) + (r - sigma**2/2) * tau
     return x
 
-def get_u_analitico_normal(x, sigma, tau, K):
-    d1 = (x + sigma**2 * (tau/2))/(sigma * np.sqrt(tau))
-    d2 = x/(sigma * np.sqrt(tau))
-    N_d1 = statistics.NormalDist.cdf(d1)
-    N_d2 = statistics.NormalDist.cdf(d2)
-    u_analitico = K * np.e**(x+sigma**2 * tau/2) * N_d1 - K * N_d2
+def get_u_analitico_normal(sigma, K, M, N, T, L):
+    u_analitico = np.zeros((N+1, M+1))
+    for j in range(M+1):
+        tau_j = update_tau_j(j, T, M)
+        for i in range(N+1):
+            x_i = update_x_i(i, L, N)
+            if j==0:
+                u_analitico[i][j] = K*max(np.e**x_i-1, 0)
+            else:
+                d1 = (x_i + sigma**2 * tau_j)/(sigma * np.sqrt(tau_j))
+                d2 = x_i/(sigma * np.sqrt(tau_j))
+                # N_d1 = statistics.NormalDist().cdf(d1)
+                # N_d2 = statistics.NormalDist().cdf(d2)
+                N_d1 = norm.cdf(d1)
+                N_d2 = norm.cdf(d2)
+                u_analitico[i][j] = K * np.e**(x_i+sigma**2 * tau_j/2) * N_d1 - K * N_d2
+                print(f"N_d1 = {N_d1} e N_d2 = {N_d2} e u_anal_ij = {u_analitico[i][j]}")
     return u_analitico
 
-def get_u_analitico(K, L, sigma, tau):  
-    u_analitico = K * np.e**(L + sigma**2 * tau/2)
-    return u_analitico
+# def get_u_analitico(K, L, sigma, tau):  
+#     u_analitico = K * np.e**(L + sigma**2 * tau/2)
+#     return u_analitico
 
 def get_S_analitico(x, tau, sigma, K, r):
     S = K * np.e**(x - (r - sigma**2 / 2) * tau)
     return S
 
-def get_V_analitico(r, T, t, sigma, S, K, metodo_u="1"): # Fazer com u da dist normal ou outro metodo analitico?
-    tau = get_tau_analitico(t, T)
-    x = get_x_analitico(S, K, r, sigma, tau)
-    if metodo_u == "1":
-        u = get_u_analitico
-    elif metodo_u == "2":
-        u = get_u_analitico_normal
+def get_V_analitico(r, T, t, sigma, S, K, M, N, L): # Fazer com u da dist normal ou outro metodo analitico?
+    u = get_u_analitico_normal(sigma, K, M, N, T, L)
     V = u * np.e**(-r * (T-t))
     return V
 
@@ -74,65 +81,88 @@ def update_S_ij(K, x_i, tau_j, r, sigma):
     S_ij = K * np.e**(x_i - (r - sigma**2/2) * tau_j)
     return S_ij
 
-def get_V_aprox(M, N, L, sigma, K, T, r, u, vetorizar=True):
+def get_V_ij(N, M, T, r, u):
     V = np.zeros((N+1, M+1))
     for j in range(M+1):
-        tau = update_tau_j(j, T, M)
-        V[:, j] = u[:, j] * np.e**(-r * tau)
+        tau_j = update_tau_j(j, T, M)
+        V[:, j] = u[:, j] * np.e**(-r * tau_j)
     return V
 
-def get_V_interpol(M, N, L, t, T, K, r, sigma, V_aprox):
-    V = np.zeros((N+1, M+1))
+def get_V_dado_S(T, M, L, N, t, S, K, r, sigma, V):
+    delta_tau = get_delta_tau_analitico(T, M)
+    delta_x = get_delta_x_analitico(L, N)
     tau = get_tau_analitico(t, T)
+    x = get_x_analitico(S, K, r, sigma, tau)
+
+    indice_i = round((x + L)/delta_x)
+    indice_j = round(tau/delta_tau)
+
+    x_i = update_x_i(indice_i, L, N)
+    x_i_prox = update_x_i(indice_i+1, L, N)
+
+    V_aprox = V[indice_i][indice_j]
+    V_interpol = ((x_i_prox - x) * V[indice_i][indice_j] - (x_i - x) * V[indice_i+1][indice_j])/(x_i_prox - x_i)
+    return V_aprox, V_interpol
+
+def preenche_conds_contorno(u, N, L, K, T, M, sigma):
+    for i in range(N+1):
+        x_i = update_x_i(i, L, N)
+        u[i][0] = K * max(np.e**x_i - 1, 0)
+    for j in range(M+1):
+        tau_j = update_tau_j(j, T, M)
+        u[N][j] = K * np.e**(L + sigma**2 * tau_j/2)
+        u[0][j] = 0
+    return u
+
+def iterador_S_ij(N, M, T, L, K, r, sigma):
+    S = np.zeros((N+1, M+1))
     for j in range(M+1):
         tau_j = update_tau_j(j, T, M)
         for i in range(N+1):
             x_i = update_x_i(i, L, N)
-            S = update_S_ij(K, x_i, tau_j, r, sigma)
-            x = get_x_analitico(S, K, r, sigma, tau)
-            V[i, j] = ((x_i[i+1] - x) * V_aprox[i][j] - (x_i[i] - x) * V_aprox[i+1][j])/(x_i[i+1] - x_i[i]) # np.divide ou soh '/'?
-    return V
+            S[i][j] = update_S_ij(K, x_i, tau_j, r, sigma)
+    return S
 
-def update_u_ij(u_ij, sigma, u_iant_j, u_iprox_j, T, M, L, N):
-    u_ij_prox = u_ij * get_delta_tau_analitico(T, M)/get_delta_x_analitico(L, N)**2 * sigma**2/2 * (u_iant_j - 2 * u_ij + u_iprox_j)
-    return u_ij_prox
-
-def update_u_ij_vetorizado(u_ij_vetor, T, M, L, N, sigma, u_iant_j_vetor, u_iprox_j_vetor):
-    u_ij_prox_vetor = u_ij_vetor + np.matmul((get_delta_tau_analitico(T, M)/get_delta_x_analitico(L, N)**2 * sigma**2/2), (u_iant_j_vetor - 2 * u_ij_vetor + u_iprox_j_vetor))
-    return u_ij_prox_vetor
-
-def iterador_u_ij(N, L, sigma, K, T, vetorizar=True):
-    M = N/L
-    u  = np.zeros((N+1, M+1))
-    if vetorizar == False:
-        for j in range(-1, M):
-            tau = update_tau_j(j, T, M)
-            for i in range(0, N+1):
-                x = update_x_i(i, L, N)
-                if i == 0:
-                    u[i][j+1] = 0
-                elif i == N:
-                    u[i][j+1] = K * np.e**(L + sigma**2 * tau/2)
-                elif j==0:
-                    u[i][j+1] = K * max(np.e**x - 1, 0)
-                else:
-                    u[i][j+1] = update_u_ij(u[i][j], sigma, u[i-1][j], u[i+1][j], T, M, L, N)
+def iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar=1):
+    u = np.zeros((N+1, M+1))
+    u = preenche_conds_contorno(u, N, L, K, T, M, sigma)
+    delta_tau = get_delta_tau_analitico(T, M)
+    delta_x = get_delta_x_analitico(L, N)
+    if vetorizar == 0:
+        for j in range(1, M+1):
+            for i in range(1, N):
+                u[i][j] = u[i][j-1] + (delta_tau /delta_x**2)*(sigma**2/2)*(u[i-1][j-1]-2*u[i][j-1]+u[i+1][j-1])
     else:
-        for i in range(N+1):
-            x = update_x_i(i, L, N)
-            u[i][0] = K * max(np.e**x - 1, 0)
+        for j in range(M):
+            u_iant_j_vetor = u[0:-2, j]
+            u_ij_vetor = u[1:-1, j]
+            u_iprox_j_vetor = u[2: , j]
 
-        for j in range(M+1):
-            tau = update_tau_j(j, T, M)
-            u[N][j] = K * np.e**(L + sigma**2 * tau/2)
-            u[0][j] = 0
-
-        for j in range(1, M):
-            tau = update_tau_j(j, T, M)
-
-            u_iant_j_vetor = u[0:N-2, j]
-            u_ij_vetor = u[1:N-1, j]
-            u_iprox_j_vetor = u[2:N, j]
-
-            u[1:N-1, j+1] = update_u_ij_vetorizado(u_ij_vetor, T, M, L, N, sigma, u_iant_j_vetor, u_iprox_j_vetor)
+            u[1:-1, j+1] = u_ij_vetor + (delta_tau/delta_x**2) * (sigma**2/2) * (u_iant_j_vetor -2 * u_ij_vetor + u_iprox_j_vetor)
     return u
+
+def gera_lucro(V, t, N, M, L, sigma, K, T, r, valor_comprado, custo_inicial):
+    k       = 100
+    S_min   = K/2
+    S_max   = 3*K/2   
+    delta_S = (S_max-S_min)/k
+    k_i     = np.arange(k)
+    S_list  = k_i*delta_S+S_min 
+    cenarios = []
+    for S in S_list:
+        resultado = get_V_dado_S(T, M, L, N, t, S, K, r, sigma, V)[1]*valor_comprado - custo_inicial
+        cenarios.append(resultado)
+    return cenarios, S_list
+
+def gera_preco_ativo(V, t, N, M, L, sigma, K, T, r):
+    k       = 10000
+    S_min   = 2*K/3
+    S_max   = 4*K/3   
+    delta_S = (S_max-S_min)/k
+    k_i     = np.arange(k)
+    S_list  = k_i*delta_S+S_min 
+    cenarios = []
+    for S in S_list:
+        resultado = get_V_dado_S(T, M, L, N, t, S, K, r, sigma, V)[1]
+        cenarios.append(resultado)
+    return cenarios, S_list
--- EP2/options.py
+++ EP2/options.py
@@ -1,39 +1,330 @@
-from cmath import tau
 from metodos import *
 
 def option(N, L, sigma, K, T, r, t, S0, vetorizar=True):
-    M = L * 2
 
-    u = iterador_u_ij(N, L, sigma, K, T, vetorizar)
-    tau = get_tau_analitico(t, T)
-    x = get_x_analitico(S0, K, r, sigma, tau)
-    u_analitico = get_u_analitico_normal(x, sigma, tau, K)
-    for j in range(M+1):
-        for i in range(N+1):
-            print("u_ij numerico = " + u[i][j])
-            print("u_ij analitico = " + u_analitico[i][j])
+    #u_ij = calcula_u(N, M, L, sigma, K, T, vetorizar)
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    print(u_ij[9000][13])
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    # u_analitico = get_u_analitico_normal(sigma, K, M, N, T, L)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    print(f"V no presente = {str(V_presente_aprox)} V no presente interpolado = {str(V_presente_interpol)}")
 
-    V_aprox = get_V_aprox(M, N, L, sigma, K, T, r, u, vetorizar)
-    V_interpol = get_V_interpol(M, N, L, t, T, K, r, sigma, V_aprox)
+def cenario_1(vetorizar):
+    N = 10000    
+    L = 10       
+    sigma = 0.01
+    K = 1.0      
+    T = 1.0      
+    r = 0.01  
+    M = 50 # condicao de estabilidade
+       
+    arquivo = open("cenario_1.txt", "w")
 
-    # S = get_S_analitico()
+    '''
+        Exercício 1
+    '''
 
+    t = 0.0   
+    S0 = 1.0  
+
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    escreve_arquivo(arquivo, N, L, M, K, sigma, T, r, S0, t, precificacao)
+    escreve_terminal( N, L, M, K, sigma, T, r, S0, t, precificacao)
+
+    '''
+        Exercício 2
+    '''
+   
+    #Grafico 1 - t = 0.5
+    t = 0.5
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_lucro(V_ij, t, N, M, L, sigma, K, T, r, valor_comprado, gasto_inicial)
+    escreve_grafico(S_list, cenarios, "Lucro e Prejuízo em t = 6 meses", "cenario_1_ex_2_g1.png")
+
+    #Grafico 2 - t = 0.25
+    t = 0.25
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 3 meses", "cenario_1_ex_2_g2.png")
+
+    #Grafico 3 - t = 0.75
+    t = 0.75
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 9 meses", "cenario_1_ex_2_g3.png")
+    
+    #Grafico 4 - t = 0.0
+    t = 0.0
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 0 meses", "cenario_1_ex_2_g4.png")
+
+    #Grafico 5 - t = T
+    t = T
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 12 meses", "cenario_1_ex_2_g5.png")
+
+    '''
+    Exercicio 3
+    '''
+    sigma = 0.02
+
+    t = 0.5    
+
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    escreve_arquivo(arquivo, N, L, M, K, sigma, T, r, S0, t, precificacao)
+    escreve_terminal( N, L, M, K, sigma, T, r, S0, t, precificacao)
+
+    #Grafico 1 - t = 0.5
+    t = 0.5
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_lucro(V_ij, t, N, M, L, sigma, K, T, r, valor_comprado, gasto_inicial)
+    escreve_grafico(S_list, cenarios, "Lucro e Prejuízo em t = 6 meses", "cenario_1_ex_3_g1.png")
+
+    #Grafico 2 - t = 0.25
+    t = 0.25
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 3 meses", "cenario_1_ex_3_g2.png")
+
+    #Grafico 3 - t = 0.75
+    t = 0.75
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 9 meses", "cenario_1_ex_3_g3.png")
+    
+    #Grafico 4 - t = 0.0
+    t = 0.0
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 0 meses", "cenario_1_ex_3_g4.png")
+
+    #Grafico 5 - t = T
+    t = T
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 12 meses", "cenario_1_ex_3_g5.png")
+
+    '''
+    Exercicio 4
+    '''
+    r = 0.1
+    sigma = 0.1
+
+    #Grafico 1 - t = 0.5
+    t = 0.5
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_lucro(V_ij, t, N, M, L, sigma, K, T, r, valor_comprado, gasto_inicial)
+    escreve_grafico(S_list, cenarios, "Lucro e Prejuízo em t = 6 meses", "cenario_1_ex_4_g1.png")
+
+    #Grafico 2 - t = 0.25
+    t = 0.25
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 3 meses", "cenario_1_ex_4_g2.png")
+
+    #Grafico 3 - t = 0.75
+    t = 0.75
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 9 meses", "cenario_1_ex_4_g3.png")
+    
+    #Grafico 4 - t = 0.0
+    t = 0.0
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 0 meses", "cenario_1_ex_4_g4.png")
+
+    #Grafico 5 - t = T
+    t = T
+    valor_comprado = 1000
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    precificacao = V_presente_interpol * 1000
+    gasto_inicial = precificacao
+    cenarios, S_list = gera_preco_ativo(V_ij, t, N, M, L, sigma, K, T, r)
+    escreve_grafico(S_list, cenarios, "Valor da opção para diferentes preços do ativo em t = 12 meses", "cenario_1_ex_4_g5.png")
+    
+    #Fim cenario 1
+    arquivo.close()
+    return
+def cenario_2(vetorizar):
+    N = 10000
+    L = 10
+    sigma = 0.1692
+    K = 5.7
+    T = 0.25
+    r = 0.1075
+    S0 = 5.6376
+    valor = 100000
+    M = 50 # condicao de estabilidade
+    
+    u_ij = iterador_u_ij(M, N, L, sigma, K, T, r, vetorizar)
+    V_ij = get_V_ij(N, M, T, r, u_ij)
+    V_presente_aprox, V_presente_interpol = get_V_dado_S(T, M, L, N, 0, S0, K, r, sigma, V_ij)
+    print(f"V no presente = {str(V_presente_aprox)} V no presente interpolado = {str(V_presente_interpol)}")
+    precificacao = V_presente_interpol * valor
+    print(f"Precificacao/premio: {precificacao}")
+
+    custo_total = precificacao + valor*S0
+    print(f"Custo total: {custo_total}")
+
+    St = 5.1604718519
+    valor_total = St * valor
+
+    lucro = valor_total - custo_total
+    print(f"Lucro em 1 de Marco com dolar a R$ 5,16: {lucro}")
+
+def cenario_3():
+    pass
+
+def escreve_arquivo(arquivo, N, L, M, K, sigma, T, r, S, t, V):
+    arquivo.write("Parâmetros:\n")
+    arquivo.write("N        = {}\n".format(N))
+    arquivo.write("L        = {}\n".format(L))
+    arquivo.write("M        = {}\n".format(M))
+    arquivo.write("K        = {:.2f}\n".format(K))
+    arquivo.write("sigma    = {:.2f}\n".format(sigma))
+    arquivo.write("T        = {:.2f}\n".format(T))
+    arquivo.write("r        = {:.2f}\n".format(r))
+    arquivo.write("S0       = {:.2f}\n".format(S))
+    arquivo.write("t        = {:.2f}\n".format(t))
+    arquivo.write("Preço da opção:\n")
+    arquivo.write("V        = {:.2f}\n".format(V))
+    arquivo.write("\n\n")
+    return
+
+def escreve_terminal(N, L, M, K, sigma, T, r, S, t, V):
+    print("Parâmetros:\n")
+    print("N        = {}\n".format(N))
+    print("L        = {}\n".format(L))
+    print("M        = {}\n".format(M))
+    print("K        = {:.2f}\n".format(K))
+    print("sigma    = {:.2f}\n".format(sigma))
+    print("T        = {:.2f}\n".format(T))
+    print("r        = {:.2f}\n".format(r))
+    print("S0       = {:.2f}\n".format(S))
+    print("t        = {:.2f}\n".format(t))
+    print("Preço da opção:\n")
+    print("V        = {:.2f}\n".format(V))
+    print("\n\n")
+    return
+
+def escreve_grafico(S_list, cenarios, titulo, nome):
+    fig, ax = plt.subplots(figsize = (11, 6))
+    ax.plot(S_list, cenarios)
+    ax.grid(visible = True, axis = "y", linestyle = "--")
+    ax.set_title(titulo)
+    ax.set_xlabel("Preço do ativo")
+    ax.set_ylabel("Valor da opção")
+    plt.savefig(nome)
+    plt.show()
 
 def main():
-    N = int(input("Insira o valor de N: "))
-    L = int(input("Insira o valor de L: "))
-    sigma = float(input("Insira o valor de sigma: "))
-    K = float(input("Insira o valor de K: "))
-    T = float(input("Insira o valor de T: "))
-    r = float(input("Insira o valor de r: "))
-    t = float(input("Insira o valor de t: "))
-    S0 = float(input("Insira o valor de S0: "))
-    St = float((input("Insira o valor de S1: ")))
+ 
 
-    option(N, L, sigma, K, T, r, t, S0, St)
+    print("#####    Escolha um cenário  #######")
+    print("##### (1) Cenário fictício   #######")
+    print("##### (2) Cenário câmbio     #######")
+    print("##### (3) Cenário real       #######")
+    print("(###################################")
+    cenario = int(input("Cenário: "))
+    print("#####    Deseja vetorizar?   #######")
+    print("##### (1) Sim                #######")
+    print("##### (0) Não                #######")
+    print("(###################################")
+    vetorizar = int(input("Escolha: "))
+
+    if cenario == 1:
+        cenario_1(vetorizar)
+    elif cenario == 2:
+        cenario_2(vetorizar)
+    elif cenario == 3:
+        cenario_3(vetorizar)
+    else:
+        print("Entrada inválida!")
 
 if __name__ == '__main__':
     try:
         main()
     except     KeyboardInterrupt:
-        print("Algo deu errado.")
+        print("Keyboard interrupt.")
